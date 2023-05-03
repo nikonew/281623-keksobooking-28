343f633787833eec0c3123630734e27c3b73ed7e
nikonew <spiritofadv@yandex.ru>
2023-05-03 22:12:12 +0300

    внес правки

--- js/template_offer.js
+++ js/template_offer.js
@@ -13,7 +13,7 @@ const offerTemplate = document.querySelector('#card').content.querySelector('.po
 const offerFragment = document.createDocumentFragment();
 
 
-offerContent.forEach((data) => {
+offerContent.slice(0,1).forEach((data) => {
   const userOffer = offerTemplate.cloneNode(true);
   userOffer.querySelector('.popup__title').textContent = data.offer.title;
   userOffer.querySelector('.popup__text--address').textContent = data.offer.adress;
@@ -21,14 +21,15 @@ offerContent.forEach((data) => {
   userOffer.querySelector('.popup__type').textContent = types[data.offer.type];
   userOffer.querySelector('.popup__text--capacity').textContent = `${data.offer.rooms} комнаты для ${data.offer.guests} гостей`;
   userOffer.querySelector('.popup__text--time').textContent = `Заезд после ${data.offer.checkin}, выезд до${data.offer.checkout}`;
-  userOffer.querySelector('.popup__features').textContent = data.offer.features;
+  userOffer.querySelector('.popup__features').alt = data.offer.features;
   const offerDescription = userOffer.querySelector('.popup__description').textContent = data.offer.description;
-  userOffer.querySelector('.popup__photos').src = data.offer.photos;
-  userOffer.querySelector('.popup__avatar').src = data.author.avatar;
+  userOffer.querySelector('.popup__photos').src = data.offer.photos.url;
+  userOffer.querySelector('.popup__avatar').src = data.author.avatar.avatar.length === 0 ? '' : data.author.avatar.avatar;
 
   if (data.offer.description === '') {
     offerDescription.classList.add('hidden');
   }
+
   offerFragment.append(userOffer);
 });
 
