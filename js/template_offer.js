816378a40d9d8982deea5de46afa4843be826c37
nikonew <121386211+nikonew@users.noreply.github.com>
2023-05-03 22:37:20 +0300

    Update template_offer.js

--- js/template_offer.js
+++ js/template_offer.js
@@ -23,7 +23,7 @@ offerContent.slice(0,1).forEach((data) => {
   userOffer.querySelector('.popup__text--time').textContent = `Заезд после ${data.offer.checkin}, выезд до${data.offer.checkout}`;
   userOffer.querySelector('.popup__features').alt = data.offer.features;
   const offerDescription = userOffer.querySelector('.popup__description').textContent = data.offer.description;
-  userOffer.querySelector('.popup__photos').src = data.offer.photos.url;
+  userOffer.querySelector('.popup__photos').src = data.offer.photos;
   userOffer.querySelector('.popup__avatar').src = data.author.avatar.avatar.length === 0 ? '' : data.author.avatar.avatar;
 
   if (data.offer.description === '') {
