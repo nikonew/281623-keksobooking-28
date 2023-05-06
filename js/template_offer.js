732135336ad67a3f9f505e80ff9c731f40606969
nikonew <spiritofadv@yandex.ru>
2023-05-06 23:59:55 +0300

    внес правки

--- js/template_offer.js
+++ js/template_offer.js
@@ -23,7 +23,7 @@ offerContent.slice(0,1).forEach((data) => {
   userOffer.querySelector('.popup__text--time').textContent = `Заезд после ${data.offer.checkin}, выезд до${data.offer.checkout}`;
   userOffer.querySelector('.popup__features').alt = data.offer.features;
   userOffer.querySelector('.popup__description').textContent = data.offer.description.length === 0 ? '' : data.offer.description;
-  userOffer.querySelector('.popup__photos').src = data.offer.photos;
+  userOffer.querySelector('.popup__photos').querySelector('.popup__photo').src = data.offer.photos;
   userOffer.querySelector('.popup__avatar').src = data.author.avatar.avatar.length === 0 ? 'img/avatars/default.png' : data.author.avatar.avatar;
 
   offerFragment.append(userOffer);
